41acac28e0366ce120a43894ce1c5b4782bfe2c7
leovt <leonhard.vogt@gmx.ch>
2020-10-21 10:18:30 +0200

    labelled output image added

--- mnist_util.py
+++ mnist_util.py
@@ -1,6 +1,6 @@
 import struct
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 def read_labels(fname):
     with open(fname, 'rb') as f:
@@ -26,4 +26,31 @@ def image_matrix(im_array):
         for x in range(w):
             ims = Image.fromarray(im_array[y][x], 'L')
             im.paste(ims, (2+x*(4+im_w), 2+y*(4+im_h)))
+    return im
+
+def labeled_image_array(images, labels, colors=None, max_width=800):
+    n, im_h, im_w = images.shape
+    font = ImageFont.truetype(r'arial.ttf', 14)
+    uq_labels = set(l for lab in labels for l in lab)
+    lbl_w = max(font.getsize(l)[0] for l in uq_labels)
+    textheight = sum(font.getmetrics())
+    box_w = 4 + max(im_w, lbl_w)
+    box_h = 4 + im_h + textheight*len(labels)
+    columns = min(n, max(1, max_width // box_w))
+    rows = (n + columns - 1) // columns
+    im = Image.new('RGB', (columns*box_w, rows*box_h), (255,255,255))
+    draw = ImageDraw.Draw(im)
+    for i, ims_data in enumerate(images):
+        ims = Image.fromarray(ims_data, 'L')
+        x = i % columns
+        y = i // columns
+        im.paste(ims, (2+x*box_w+(box_w-im_w)//2, 2+y*box_h))
+        for k, lbl in enumerate(labels):
+            if colors is not None:
+                fg, bg = colors[k][i]
+                bbox = draw.textbbox((2+x*box_w+box_w//2, 4+y*box_h+im_h+k*textheight), lbl[i], font=font, anchor='mt')
+                draw.rectangle(bbox, fill=bg)
+            else:
+                fg = 0
+            draw.text((2+x*box_w+box_w//2, 4+y*box_h+im_h+k*textheight), lbl[i], font=font, anchor='mt', fill=fg)
     return im
